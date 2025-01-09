e74fa4d81ac1c7824b6564709215b154fbd2823d
chetnagupta <chetnaguptaa24@gmail.com>
2025-01-09 23:56:48 +0530

    Question 20 half done

--- patterns/pattern14.cpp
+++ patterns/pattern14.cpp
@@ -11,10 +11,12 @@ using namespace std;
 
 int main(){
     int n = 5;
-    cout<<1<<endl;
-    for(int i = 1; i <= n; i++){
+    int count = 1;
+   
+    for(int i = 0; i < n; i++){
         for(int j = 0; j <= i; j++){
-            cout << 2*i+j << " ";
+            cout << count << " ";
+            count++;
         }
         cout<<endl;
     }
--- patterns/pattern20.cpp
+++ patterns/pattern20.cpp
@@ -5,19 +5,23 @@ int main(){
     
     int n = 5;
     for(int i = 0; i< n; i++){
+        // step 1: printing stars
         for(int j = 0; j < n-i ; j++){
-            cout << "* ";
+            cout << "*";
         }
+        // step 2: spaces
        for(int j = 0; j < 2*i; j++){
         cout << " ";
        }
-       for(int j = 0; j < n-i; j++){
-        cout << "* ";
-       }
+       // step 3: stars
+       for(int j = 0; j < n-i ; j++){
+            cout << "*";
+        }
 
-       cout << endl;
-       
+       cout << endl; 
     }
+      
+    
     
     
     return 0;
