e3062697696bc785ec07db040c66f7f5b229050e
chetnagupta <chetnaguptaa24@gmail.com>
2025-01-10 01:30:15 +0530

    Question 21 done

--- patterns/pattern21.cpp
+++ patterns/pattern21.cpp
@@ -4,14 +4,49 @@ using namespace std;
 int main(){
 
     int n = 5;
-    for(int i = 0; i < 2*n; i++){
-        for(int j = 0; j <= 2*i-1; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j <= i; j++){
 
-            cout << "*";
-            
-            if()
+            cout << "*"; 
+        }
+        for(int j = 0; j < 2*n-2*i-2; j++){
+            cout << " ";
+        }
+        for(int j = 0; j <= i; j++){
+
+            cout << "*"; 
         }
         cout << endl;
     }
+    for(int i = n-2; i >= 0; i--){
+        for(int j = 0; j <= i; j++){
+
+            cout << "*"; 
+        }
+        for(int j = 0; j < 2*n-2*i-2; j++){
+            cout << " ";
+        }
+        for(int j = 0; j <= i; j++){
+
+            cout << "*"; 
+        }
+        cout << endl;
+    }
+    
     return 0;
 }
+/*
+
+*        * 0
+**      ** 1
+***    *** 2
+****  **** 3 n-2
+********** 4 n-1
+
+****  **** 3 n-2
+***    *** 2
+**      ** 1
+*        * 0 
+
+
+*/
